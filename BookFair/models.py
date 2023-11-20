0b269e95c709cd79a15226faab95f645ac09b94d
Phillipmoore01 <phillipmoore3102@gmail.com>
2023-11-20 11:14:47 -0800

    Models and URL
    added Cart and CartItem models
    started editing the url to get the images

--- BookFair/models.py
+++ BookFair/models.py
@@ -6,7 +6,8 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.control.auth.models import User
+import uuid
 
 class Category(models.Model):
     cat_id = models.PositiveIntegerField(primary_key=True)
@@ -86,3 +87,20 @@ class Product(models.Model):
     class Meta:
 #        managed = False
         db_table = 'PRODUCT'
+
+    class Cart(models.Model):
+     #   id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+        id = models.PositiveIntegerField(primary_key=True)
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        completed = models.BooleanField(default=False)
+
+        def __str__(self):
+            return str(self.id)
+    
+    class CartItem(models.Model):
+        product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+        cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cartitems")
+        quantity = models.IntegerField(default=0)
+
+        def __str__(self):
+            return self.product.name
